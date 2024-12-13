e9c664431db66bf55d73a29ff72eefff1245d8f5
Mahmood-Anaam <eng.mahmood.anaam@gmail.com>
2024-12-13 22:20:04 +0300

    update install requires

--- setup.py
+++ setup.py
@@ -13,35 +13,11 @@ setup(
     package_dir={"": "src"},
 
     install_requires=[
-        
-        "torch>=1.13.1",
-        "torchvision>=0.14.1",
-        "transformers>=4.12.0",
-        "pytorch-transformers>=1.2.0",
-        "numpy>=1.23.5",
-        "pandas>=1.1.5",
-        "scipy>=1.5.4",
-        "scikit-learn>=0.24.2",
-        "opencv-python>=4.5.3",
-        "Pillow>=8.3.2",
-        "matplotlib>=3.4.3",
-        "tqdm>=4.62.3",
-        "anytree>=2.12.1",
-        "yacs>=0.1.8",
-        "pycocotools",
-        "timm",
-        "einops",
-        "PyYAML>=5.4.1",
-        "cython",
-        "ninja",
-        "clint>=0.5.1",
-        "cityscapesScripts>=2.2.4",
-        "h5py",
-        "nltk",
-        "joblib",
-        "ipython",
-        "ipykernel==5.5.6",
-
+        "transformers==4.46.3",
+        "torch==2.5.1+cu121",
+        "datasets==2.14.4",
+        "fsspec==2024.10.0",
+        "numpy==1.26.4",
         ],
 
     classifiers=[
@@ -52,7 +28,4 @@ setup(
     python_requires=">=3.9",
 )
 
-# Install the sub-package first
 
-
-# restart_kernel()
